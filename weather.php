a33fd6d7355831ca55f059fdca5e72f772f8c9aa
Yuzhen299 <yche0358@student.monash.edu>
2021-08-26 14:42:18 +1000

    Add the style for weather php file

--- weather.php
+++ weather.php
@@ -6,16 +6,10 @@ $db ="csv_db";
 
 $db =new mysqli('127.0.0.1:55937', $user, $pass, $db) or die ("Unable to connect");
 
-echo "Success: A proper connection to MySQL was made!";
+
+//echo"great work!!";
 
 
-echo"great work!!";
-
-echo '<br>';
-
-echo '<style media="screen" type="text/css">@font-face{font-family:FontAwesome;src:url(/css/fonts/fontawesome-webfont);font-weight:400;font-style:normal}.fa{display:inline-block;font:normal normal normal 14px/1 FontAwesome;font-size:inherit;text-rendering:auto;-webkit-font-smoothing:antialiased;-moz-osx-font-smoothing:grayscale}.fa-angle-double-up:before{content:"\f102"}</style>';
-
-echo '<br>';
 
 $search_value=$_POST["search"];
 
@@ -31,12 +25,12 @@ if ($lo->num_rows > 0 ) {
   // output data of each row
   while($row1 = $lo->fetch_assoc()) {
     $new_long =  $row1["longitude"];
-    echo  " - long: " . $row1["longitude"]. "<br>";
+   // echo  " - long: " . $row1["longitude"]. "<br>";
   }
     // output data of each row
   while($row = $la->fetch_assoc()) {
     $new_lat =  $row["lat"];
-    echo  " - lat: " . $row["lat"]. "<br>";
+   // echo  " - lat: " . $row["lat"]. "<br>";
   }
   $SuburbApi = 'http://api.openweathermap.org/geo/1.0/zip?zip='.$search_value.',au&appid=f8602df67c495efda45f5097b5244ba6';
 
@@ -57,8 +51,12 @@ $Sub_data = json_decode($sub_response);
 $postcode = $Sub_data ->zip;
 $suburb = $Sub_data ->name;
 
-echo 'Postcode: '.$postcode. "<br>";
-echo 'Suburb: '.$suburb. "<br>"; 
+$output .="<div>". $suburb."</div>";
+
+//echo 'Postcode: '.$postcode. "<br>";
+//echo 'Suburb: '.$suburb. "<br>"; 
+
+
 
 
 
@@ -78,7 +76,7 @@ curl_close($ch);
 $data = json_decode($response);
 
 
-echo '<br>';
+//echo '<br>';
 $Time = $data ->current ->dt;
 $temperature = $data ->current ->temp;
 $humidity = $data -> current ->humidity;
@@ -87,13 +85,13 @@ $weather = $data ->current ->weather[0]->description;
 $main_weather = $data ->current ->weather[0]->main;
 $dateInLocal = date("Y-m-d", $Time);
 
-
+/*
 echo 'Time: '.$dateInLocal. "<br>";
 echo 'temperature: '.$temperature.'°C'. "<br>";
 echo 'humidity: '.$humidity.'%'. "<br>"; 
 echo 'wind speed: '.$wind_speed.' m/s'. "<br>";
 echo 'weather: '.$weather. "<br>";
-
+*/
 
 $air_api = 'http://api.openweathermap.org/data/2.5/air_pollution?lat='.$new_lat.'&lon='.$new_long.'&appid=f8602df67c495efda45f5097b5244ba6';
 
@@ -111,12 +109,12 @@ curl_close($ch);
 $air_data = json_decode($air_response);
 $air_qi = $air_data ->list[0] ->main ->aqi;
 
-echo '<br>';
-echo 'Air quality: '.$air_qi. "<br>";
+//echo '<br>';
+//echo 'Air quality: '.$air_qi. "<br>";
 
 # temperature suggestion
-echo '<br>';
-
+//echo '<br>';
+/*
 if ($temperature <= 10) {
   echo "Temperature Suggestion: "."<br>";
   echo "·Build hives in almond plantations where temperatures are warmer"."<br>";
@@ -207,7 +205,7 @@ else {
 
 
 
-
+*/
 
 
 
@@ -218,3 +216,278 @@ else {
 
 else {echo "<script>alert('Not correct postcode'); location.href = 'about.html'</script>";
 }
+
+
+
+
+?> 
+
+
+
+
+
+<!DOCTYPE html>
+<!--[if IE 7]>    <html class="no-js ie7 oldie" lang="en-US"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js ie8 oldie" lang="en-US"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html lang="en">
+
+<head>
+
+    <!--meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--meta tags ends-->
+
+    <title>Beenefit</title>
+
+    <!--- Links to google fonts -->
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,800%7cRoboto+Mono:400,700%7cMerriweather:300%7cAbril+Fatface'
+          rel='stylesheet'>
+    <!-- Links to fonts ends -->
+
+    <!-- Bootstrap stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Icons -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!-- Popup Images -->
+    <link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+
+    <!-- css animation -->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <!-- custom stylesheets -->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+<style>
+
+
+.report-container {
+    border: #E0E0E0 1px solid;
+    padding: 20px 40px 40px 40px;
+    border-radius: 2px;
+    width: 550px;
+    margin: 0 auto;
+}
+
+.weather-icon {
+    vertical-align: middle;
+    margin-right: 20px;
+}
+
+.weather-forecast {
+    color: #212121;
+    font-size: 1.2em;
+    font-weight: bold;
+    margin: 20px 0px;
+}
+
+span.min-temperature {
+    margin-left: 15px;
+    color: #929292;
+}
+
+.time {
+    line-height: 25px;
+}
+</style>
+  
+
+
+</head>
+
+<body>
+
+<!-- Start: Preloader section -->
+<div id="loader-wrapper">
+    <div id="loader"></div>
+</div>
+<!-- End: Preloader section -->
+
+<!-- DOCUMENT WRAPPER STARTS -->
+    <main>
+
+        
+
+        <!-- MAIN HEADER STARTS-->
+        <header id="header">
+
+            <!-- TOP NAVIGATION -->
+            <nav class="top-navigation-bar navbar navbar-default navbar-fixed-top">
+                <div class="container">
+                <div class="row">
+                <div class="col-md-12">
+                    <!-- Brand and toggle get grouped for better mobile display -->
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" 
+                                data-toggle="collapse" data-target="#top-navigation-bar" 
+                                aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="#">BEENEFIT<span class="thin"></span></a>
+                    </div>
+
+                    <!-- Collect the nav links, forms, and other content for toggling -->
+                    <div class="collapse navbar-collapse" id="top-navigation-bar">
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="index.html">Home</a></li>
+                            <li><a href="about.html">Bee Aware of Weather</a></li>
+                            <!-- <li><a href="contact.html">Get in Touch</a></li>                             -->
+                        </ul>
+                    </div>
+                    <!-- /.navbar-collapse -->
+                    </div>
+                    </div>
+                </div>
+                <!-- /.container-->
+            </nav>
+            <!-- TOP NAVIGATION ENDS -->
+
+        </header>
+        
+        <!-- main header ends-->
+
+
+        <section id="about-banner" class="about-banner">
+          <div class="container">
+            <div class="about-banner-wrapper full-height">
+            <div class="about-heading">
+              <div class="report-container">
+                  <h2><?php echo $suburb; ?> Weather Status</h2>
+                  <div class="time">
+                      <div><?php echo "time", $dateInLocal; ?></div>
+                      
+                  </div>
+                  <div class="weather-forecast">
+                              </div>
+                  <div class="time">
+                      <div>Humidity: <?php echo $humidity; ?> %</div>
+                      <div>Wind: <?php echo $wind_speed; ?> m/s</div>
+                  </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+
+        </section>
+
+
+
+        <section id="address" class="address page-top">
+            <div class="container">
+                <div class="row">
+                    
+                        <div class="col-md-6">
+                        <div class="address-wrapper wow fadeInUp" data-wow-delay="0.3s">
+                        <h1>Our Address</h1>
+                        <table class="address-table">
+                                <tbody>
+                                    <tr>
+                                        <td>Location</td>
+                                        <td>House No.124, Road No.1, Mohakhali<br>Dhaka 1206, Bangladesh</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Phone:</td>
+                                        <td>+8801922556041</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Email:</td>
+                                        <td>ghughu@gmail.com</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Facebook:</td>
+                                        <td>facebook.com/pages/ghughu</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            </div>
+                    </div>
+                    <div class="col-md-6">
+                    <div class="map-wrapper wow fadeInUp" data-wow-delay="0.6s">
+                        <div id="map"></div>                     
+                    </div>
+                    </div>
+                    
+                    
+            </div>
+            </div>
+        </section>
+
+       
+
+
+         <!-- footer section starts -->
+        <footer id="footer" class="footer">
+            <div class="container wow fadeInUp">
+                <div class="row">
+                    <div class="footer-wrapper clearfix">
+                    <div class="col-md-6 footer-left">
+                        <div class="row">
+                            <div class="col-xs-6">
+                                <div class="footer-link footer-link-1">
+                                    <h4>Create Free Account</h4>
+                                    <ul>
+                                        <li><a href="#">View courses</a></li>
+                                        <li><a href="#">Discuss</a></li>
+                                        <li><a href="#">Suggest course</a></li>
+                                        <li><a href="#">View courses</a></li>
+                                        <li><a href="#">Discuss</a></li>
+                                        <li><a href="#">Suggest course</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col-xs-6">
+                                <div class="footer-link footer-link-2">
+                                    <h4>Create Free Account</h4>
+                                    <ul>
+                                        <li><a href="#">View courses</a></li>
+                                        <li><a href="#">Discuss</a></li>
+                                        <li><a href="#">Suggest course</a></li>
+                                        <li><a href="#">View courses</a></li>
+                                        <li><a href="#">Discuss</a></li>
+                                        <li><a href="#">Suggest course</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-5 col-md-offset-1 footer-text">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip</p>
+                        <span>&copy; 2016 G&amp;G. Theme By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a></span>
+                    </div>
+                    </div>
+                </div><!-- End: .row -->
+            </div><!-- End: .container-->
+        </footer>
+        <!-- footer section ends -->
+
+    </main>
+<!-- DOCUMENT WRAPPER ENDS -->
+
+
+
+<!-- SCRIPTS -->
+
+    <!-- jQuery (necessary for all the plugins) -->
+    <script src="js/jquery-1.11.2.min.js"></script>
+
+    <script src="http://maps.google.com/maps/api/js"></script>
+    <script src="js/gmaps.js"></script>
+    <script type="text/javascript" src="js/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="js/jquery.easing.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/jquery.validate.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+
+<!-- SCRIPTS ENDS -->
+</body>
+
+</html>
+
