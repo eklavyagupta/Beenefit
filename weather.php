face8a0e07232b38341c2b73c87195de09abeac9
guochengggg <63344791+guochengggg@users.noreply.github.com>
2021-08-26 22:18:48 +1000

    add favicon in php file
    add favicon in php file

--- weather.php
+++ weather.php
@@ -252,6 +252,11 @@ else {echo "<script>alert('Not correct postcode'); location.href = 'about.html'<
     <!--meta tags ends-->
 
     <title>Beenifit</title>
+    <!-- Links to favicon -->
+    <link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="images//favicon-16x16.png">
+    <link rel="manifest" href="images/site.webmanifest">
     <!-- Links to logo fonts -->
     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Bangers" />
     
