8637cc5f1dc6df4c7917f2ffa447e62d476a9b23
Eklavya Gupta <eklavyagupta@live.com>
2021-09-07 04:20:23 +1000

    testing.

--- public/email.php
+++ public/email.php
@@ -1,7 +1,7 @@
 <?php
 
-$user ="azure";
-$pass ='6#vWHD_$';
+$user ="root";
+$pass ='';
 $db ="localdb";
 
 $db =new mysqli('127.0.0.1:50190', $user, $pass, $db) or die ("Unable to connect");
@@ -10,6 +10,9 @@ $email_input=$_POST["email"];
 $postcode_input=$_POST["postcode"];
 
 
+$sql="INSERT INTO `beekeeper` ('id', 'email', 'postcode') VALUES (NULL, '$email_input','$postcode_input')";
+
+$bk = $connection->query($sql);
 
 echo "<script>alert('Subscribed successfully'); location.href = 'index.html'</script>";
 
