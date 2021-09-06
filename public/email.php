88bb4e079472a1b1643167d1b7fcd2f4f2ba7479
Yuzhen299 <yche0358@student.monash.edu>
2021-09-06 22:00:51 +1000

    add subscribe email and postcode into database

---
+++ public/email.php
@@ -0,0 +1,20 @@
+<?php
+
+$user ="azure";
+$pass ='6#vWHD_$';
+$db ="localdb";
+
+$db =new mysqli('127.0.0.1:50190', $user, $pass, $db) or die ("Unable to connect");
+
+$email_input=$_POST["email"];
+$postcode_input=$_POST["postcode"];
+
+
+$sql="INSERT INTO `beekeeper` (email,postcode) VALUES ('$email_input','$postcode_input')";
+
+$lo = $connection->query($sql);
+
+echo "<script>alert('Subscribed successfully'); location.href = 'index.html'</script>";
+
+ ?>
+
