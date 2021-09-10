8c80de253251915acb7307613b43ef840498189b
Yuzhen299 <yche0358@student.monash.edu>
2021-09-10 10:59:47 +1000

    input alert

--- public/email.php
+++ public/email.php
@@ -9,12 +9,38 @@ $db =new mysqli('127.0.0.1:50190', $user, $pass, $db) or die ("Unable to connect
 $email_input=$_POST["email"];
 $postcode_input=$_POST["postcode"];
 
+if (strlen($postcode_input) == 4 & preg_match("/^[a-zA-Z-' ]*$/",$email_input){
+    $sql1= "select longitude from `postcodes` where postcode = '$postcode_input'";
 
-$sql="INSERT INTO `beekeeper` (email, postcode) VALUES ('$email_input', '$postcode_input')";
+    $lo = $db->query($sql1);
 
-$bk = $db->query($sql);
+    $sql2= "select lat from `postcodes` where postcode = '$postcode_input'";
 
-echo "<script>alert('Subscribed successfully'); location.href = 'index.html'</script>";
+    $la = $db->query($sql2);
+
+    if ($lo->num_rows > 0 ) {
+    // output data of each row
+    $sql="INSERT INTO `beekeeper` (email, postcode) VALUES ('$email_input', '$postcode_input')";
+
+    $bk = $db->query($sql);
+
+    echo "<script>alert('Subscribed successfully'); location.href = 'index.html'</script>";
+
+    }
+    else {
+        echo "<script>alert('Not correct postcode!'); location.href = 'index.htm#sub'</script>";
+    }
+    
+
+}
+
+else {
+    echo "<script>alert('Not correct email or postcode!'); location.href = 'index.htm#sub'</script>";
+}
+
+
+
+
 
  ?>
 
