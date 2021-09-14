e376533d949118126e02a3bfa0df6ce2198601d9
Eklavya Gupta <eklavyagupta@live.com>
2021-09-14 17:44:50 +1000

    testing

--- public/weather.php
+++ public/weather.php
@@ -272,9 +272,10 @@ span.min-temperature {
                 </div>
                 
                 <div class="weather-forecast">
+                    <p>
                     <img
                         src="http://openweathermap.org/img/wn/<?php echo $weather_icon; ?>@4x.png" class="weather-icon" style="vertical-align:middle" /> 
-                        <h1><?php echo $temperature; ?>°C</h1>
+                        <h1><?php echo $temperature; ?>°C</h1></p>
                         <span class="min-temperature"><h3><?php echo $weather; ?></h3></span>
                 </div>
                 </br>
