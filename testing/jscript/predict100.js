5f1f18478fca2c18004c466a1a67f4ded4268e71
Yuzhen299 <yche0358@student.monash.edu>
2021-10-06 12:50:58 +1100

    test

--- testing/jscript/predict100.js
+++ testing/jscript/predict100.js
@@ -44,7 +44,7 @@ $("#image-selector").change(function () {
 
 let model;
 (async function () {
-	model = await tf.loadModel('file://./model_kerasnative_v4/model.json');
+	model = await tf.loadModel('./model_kerasnative_v4/model.json');
 	$("#selected-image").attr("src", "http://bee.test.woza.work/assets/008_266.png")
 	
 	
