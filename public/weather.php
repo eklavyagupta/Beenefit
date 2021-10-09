a0e74d156cd701bdb16814b07b7752923560567c
Yuzhen299 <yche0358@student.monash.edu>
2021-10-09 12:17:39 +1100

    add space

--- public/weather.php
+++ public/weather.php
@@ -321,30 +321,30 @@ else {echo "<script>alert('Not correct postcode'); location.href = 'index.html'<
                       // print the suggestion based on different weather situation
                       else {
                         if ($temperature <= 10) {
-                          echo"· Close off the screened-bottom board when the hive in open plains "."<br>".
-                        "· Provide a little insulation to the outside of their hives if possible: like terrarium heaters"."<br>".
-                        "· Make sure there aren't drafty holes in their equipment that let cold air or, worse, water in."."<br>".
-                        "· Hives are distributed evenly throughout the orchard individually"."<br>t";
+                          echo"· Close off the screened-bottom board when the hive in open plains "."<br>"."<br>".
+                        "· Provide a little insulation to the outside of their hives if possible: like terrarium heaters"."<br>"."<br>".
+                        "· Make sure there aren't drafty holes in their equipment that let cold air or, worse, water in."."<br>"."<br>".
+                        "· Hives are distributed evenly throughout the orchard individually"."<br>"."<br>";
                         }
 
                         elseif ($temperature > 10 & $temperature < 30 ){
                           echo 
-                          " · Your bees are happy with current temperature. reminders: to refill the water feeder three times a week"."<br>";
+                          " · Your bees are happy with current temperature. reminders: to refill the water feeder three times a week"."<br>"."<br>";
                         }
                         
   
                         elseif ($temperature > 35 & $temperature < 37){
-                          echo "· Hive inspection should be postponed during extreme hot days"."<br>".
-                          " · Provide broken shade over the hives(note that avoid total shade for the whole day"."<br>".
-                          " · Ideally clean all the imflammable materials like grass and leaves around the hives"."<br>".
-                          "· Spraying water on the external walls of the hive"."<br>";
+                          echo "· Hive inspection should be postponed during extreme hot days"."<br>"."<br>".
+                          " · Provide broken shade over the hives(note that avoid total shade for the whole day"."<br>"."<br>".
+                          " · Ideally clean all the imflammable materials like grass and leaves around the hives"."<br>"."<br>".
+                          "· Spraying water on the external walls of the hive"."<br>"."<br>";
                         }
                         elseif ($temperature > 37 ){
-                          echo "· Hive inspection should be postponed during extreme hot days"."<br>".
-                          " · Place a spare hive cover on top of the hive cover (a sheet of hardboard or a car windscreen heat shield can be used for shading of side of the hive)"."<br>".
-                          " · Place bricks under the hives so the hives is around 150 mm from the ground"."<br>".
-                          " · Spraying water on the external walls of the hive"."<br>".
-                          " · Recommended to refill the water feeder on a daily base"."<br>";
+                          echo "· Hive inspection should be postponed during extreme hot days"."<br>"."<br>".
+                          " · Place a spare hive cover on top of the hive cover (a sheet of hardboard or a car windscreen heat shield can be used for shading of side of the hive)"."<br>"."<br>".
+                          " · Place bricks under the hives so the hives is around 150 mm from the ground"."<br>"."<br>".
+                          " · Spraying water on the external walls of the hive"."<br>"."<br>".
+                          " · Recommended to refill the water feeder on a daily base"."<br>"."<br>";
                         }
                         
   
@@ -355,7 +355,7 @@ else {echo "<script>alert('Not correct postcode'); location.href = 'index.html'<
   
   
                         if ($air_qi >= 3) {
-                          echo"· Put a net cover on top if possible."."<br>";
+                          echo"· Put a net cover on top if possible."."<br>"."<br>";
                           
                         } 
   
@@ -364,7 +364,7 @@ else {echo "<script>alert('Not correct postcode'); location.href = 'index.html'<
                         # Rain suggestion
   
                         if ($main_weather == "Rain"|$main_weather == "Thunderstorm" |$main_weather == "Drizzle") {
-                          echo "· Build a full hive"."<br>"." · Hives are distributed evenly throughout the orchard individually."."<br>";
+                          echo "· Build a full hive"."<br>"." · Hives are distributed evenly throughout the orchard individually."."<br>"."<br>";
                           
                         } 
   
@@ -377,8 +377,8 @@ else {echo "<script>alert('Not correct postcode'); location.href = 'index.html'<
   
   
                         if ($humidity >80 |$humidity<75) {
-                          echo " · Forego upper ventilation or quilts and simply place a thick sheet of nonporous insulation (e.g. 2″-4″ thick styrofoam) above the inner cover"."<br>".
-                          " · Or rear shim tilting the hive forward about 3 degrees"."<br>";
+                          echo " · Forego upper ventilation or quilts and simply place a thick sheet of nonporous insulation (e.g. 2″-4″ thick styrofoam) above the inner cover"."<br>"."<br>".
+                          " · Or rear shim tilting the hive forward about 3 degrees"."<br>"."<br>";
                           
                         }
   
@@ -388,7 +388,7 @@ else {echo "<script>alert('Not correct postcode'); location.href = 'index.html'<
   
   
                         if ($wind_speed > 5.5) {
-                          echo "· Place bees in places with frequent winds"."<br>"." · Ensure The location of the bee farm should be set at the downwind of the honey powder source, so that the bees will go against the wind when they are out of the nest without load.";
+                          echo "· Place bees in places with frequent winds"."<br>"."<br>"." · Ensure The location of the bee farm should be set at the downwind of the honey powder source, so that the bees will go against the wind when they are out of the nest without load.";
                           
                         }
                       }
