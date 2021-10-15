5d16e31f1f72835e4894108473c2e4e712511b05
Yuzhen299 <yche0358@student.monash.edu>
2021-10-15 14:17:17 +1100

    model link

--- public/assets/js/predict100.js
+++ public/assets/js/predict100.js
@@ -150,7 +150,7 @@ $("#pre-selector").change(function ()  {
 let model;
 (async function () {
 	
-	model = await tf.loadModel('https://beenefit.studio/model/model.json');
+	model = await tf.loadModel('https://raw.githubusercontent.com/Yuzhen299/test/master/model.json');
 	$("#selected-image").attr("src", "assets/img/illustrations/passion.png")
 	
 	
