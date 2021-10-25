8ba271559d95e42fdef42c1defc68df2374d8731
Eklavya Gupta <eklavyagupta@live.com>
2021-10-25 11:25:23 +1100

    DB IP changed sendemail.

--- public/sendemail.php
+++ public/sendemail.php
@@ -4,7 +4,7 @@ $user ="azure";
 $pass ='6#vWHD_$';
 $db ="localdb";
 
-$db =new mysqli('127.0.0.1:50190', $user, $pass, $db) or die ("Unable to connect");
+$db =new mysqli('127.0.0.1:49297', $user, $pass, $db) or die ("Unable to connect");
 
 // sql query
 $weather_check= mysqli_query($db,"select lat, longitude from `beekeeper`b,`postcodes`p where p.postcode = b.postcode");
